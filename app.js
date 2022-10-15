40b5bfea9d4046e876a7722d9dc70f51d89ed0cb
Jones Tweneboah <tweneboahjones07@gmail.com>
2022-10-15 21:06:01 +0100

    New changes

--- app.js
+++ app.js
@@ -112,11 +112,11 @@ app.post("/", (req, res) => {
 	
 });
 
-/*let port = process.env.PORT;
+let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
-}*/
+}
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log("Server is running on port 3000");
 });
